68be0b15ec8e95e1c697911bcc0a1665b233421d
Klosiek <46522348+Klosiek@users.noreply.github.com>
2019-03-28 13:27:42 +0100

    - Dodanie readWorkBook + biblioteki XSSF

---
+++ src/readWorkBook.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.*;
+
+public class readWorkBook {
+    public static void read()throws Exception{
+
+        FileInputStream out = new FileInputStream(new File("Database.xlsx"));
+
+        XSSFWorkbook workbook = new XSSFWorkbook(out);
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        FormulaEvaluator formula = workbook.getCreationHelper().createFormulaEvaluator();
+
+        for(Row row: sheet){
+            for(Cell cell : row){
+                switch (formula.evaluateInCell(cell).getCellType()){
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "\t\t\t");
+                        break;
+                    case STRING:
+                        System.out.print(cell.getStringCellValue() + "\t\t\t");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args)throws Exception {
+        read();
+    }
+}
