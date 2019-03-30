565dd10813b5e18b48fe36cf3a4c0946e2be4458
Klosiek <46522348+Klosiek@users.noreply.github.com>
2019-03-30 18:52:29 +0100

    .

--- src/writeWorkBook.java
+++ src/writeWorkBook.java
@@ -8,14 +8,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class writeWorkBook {
     public static void write()throws IOException{
         String excelFileName = "C:/Users/Klosiek/Desktop/AiSD_Projekt/Database_stats.xlsx";
-        String sheetName = "Sheet1";
+        String sheetName = "Sheet1p";
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet(sheetName);
 
         for (int i = 0; i < 5; i++ ) {
             XSSFRow row = sheet.createRow(i);
 
-            //iterating c number of columns
             for (int j = 0; j < 5; j++ ) {
                 XSSFCell cell = row.createCell(j);
 
